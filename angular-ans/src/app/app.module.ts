af54cb2972864f5b8a336b1ba9a34b44a4842f04
Dhana Shekhar Tontanahal <dhanashekhar.tontanahal@zemosolabs.com>
2020-12-06 03:59:35 +0530

    add customSort pipe , implementation , data

--- angular-ans/src/app/app.module.ts
+++ angular-ans/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RepeaterComponent } from './repeater/repeater.component';
+import { CustomSortPipe } from './custom-sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepeaterComponent
+    RepeaterComponent,
+    CustomSortPipe
   ],
   imports: [
     BrowserModule
