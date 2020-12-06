17cd5b203f67cccf9c2ace989e1809d40ad17df5
Dhana Shekhar Tontanahal <dhanashekhar.tontanahal@zemosolabs.com>
2020-12-06 08:59:04 +0530

    add changes to sorting to include desd order

--- angular-ans/src/app/custom-sort.pipe.ts
+++ angular-ans/src/app/custom-sort.pipe.ts
@@ -6,13 +6,20 @@ import { orderBy } from 'lodash';
 export class CustomSortPipe implements PipeTransform {
 
   transform(value: any[], order = '', column: string = ''): any[] {
-    if (!value || order === '' || !order) { return value; } // no array
+    // if (!value || order === '' || !order) { return value; } // no array
+    let orderElemBy = "";
+    if (column.substring(0, 1) == '-') {
+      column = column.substring(1, column.length);
+      orderElemBy = 'desc'
+    } else {
+      orderElemBy = 'asc'
+    }
     if (value.length <= 1) { return value; } // array with only one item
-    if (!column || column === '') { 
-      if(order==='asc'){return value.sort()}
-      else{return value.sort().reverse();}
+    if (!column || column === '') {
+      if (orderElemBy === 'asc') { return value.sort() }
+      else { return value.sort().reverse(); }
     } // sort 1d array
-    return orderBy(value, [column], [order]);
+    return orderBy(value, [column], [orderElemBy]);
   }
 
 }
